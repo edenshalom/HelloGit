b04a70d70b1f102c8d4a54b68655966e66110e00
Eden Shalom <eden1971@gmail.com>
2015-10-01 12:47:26 +0300

    igmore file and test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		//comment 1
+		//comment 2
 	}
 
 }
